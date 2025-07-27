2a413a19080a228ab9fe76070e0e6ceb3f82edd9
Hafidz <hafiiidz@gmail.com>
2025-07-27 22:11:46 +0700

    Sleep Tracker Feature: Clean up DailySleepSummary model

--- app/models/daily_sleep_summary.rb
+++ app/models/daily_sleep_summary.rb
@@ -1,3 +1,8 @@
 class DailySleepSummary < ApplicationRecord
   belongs_to :user
+  
+  validates :date, presence: true
+  validates :total_sleep_duration_minutes, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :number_of_sleep_sessions, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :user_id, uniqueness: { scope: :date, message: "already has a summary for this date" }
 end
